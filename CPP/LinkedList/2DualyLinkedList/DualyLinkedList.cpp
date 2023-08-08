b30b5d5267fd049a30fa813e2c172bc6242210ff
Questi0nM4rk <56120540+Questi0nM4rk@users.noreply.github.com>
2023-08-08 18:48:23 +0200

    added

---
+++ CPP/LinkedList/2DualyLinkedList/DualyLinkedList.cpp
@@ -0,0 +1,22 @@
+#include "DualyLinkedList.h"
+
+
+//add node
+Node* addNode(const int &val){
+
+}
+
+//rem node
+bool remNode(const int &val){
+
+}
+
+//insert node
+Node* insertNode(const int &pos, const int &val){
+
+}
+
+//print list
+void printList(){
+        
+}
