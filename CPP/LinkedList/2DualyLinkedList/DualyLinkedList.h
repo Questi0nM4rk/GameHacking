884b1083705e808693fe97f5a2bdcd0a879375a6
Questi0nM4rk <56120540+Questi0nM4rk@users.noreply.github.com>
2023-08-20 01:31:10 +0200

    change

--- CPP/LinkedList/2DualyLinkedList/DualyLinkedList.h
+++ CPP/LinkedList/2DualyLinkedList/DualyLinkedList.h
@@ -23,25 +23,59 @@ struct Node
 class DualyLinkedList
 {
 private:
-    uptr<Node> head;
-    uptr<Node> tail;
+    uptr<Node> head = nullptr;
+    uptr<Node> tail = nullptr;
+    int size = 0;
 
 public:
     
-    //intit
-    DualyLinkedList() : head(nullptr), tail(nullptr) {}
+    DualyLinkedList() : head(nullptr), tail(nullptr), size(0) {}
 
-    //add node
-    Node* addNode(const int &val);
+    Node* addNodeStart(const int &val); //done C
 
-    //rem node
-    bool remNode(const int &val);
+    Node* addNodeEnd(const int &val);   //done C
 
-    //insert node
-    Node* insertNode(const int &pos, const int &val);
+    Node* insertNode(const int &pos, const int &val);   //done C
 
-    //print list
-    void printList();
+    Node* getNodeByVal(const int &val);  //done C-find
+
+    Node* getNodeByPos(const int &pos);  //done
+
+    Node* getHead() const;  //done
+
+    Node* getTail() const;  //done
+
+    uptr<Node> popNodeByVal(const int &val); //done C-find
+   
+    uptr<Node> popNodeByPos(const int &pos); //done
+
+    uptr<Node> popNode(Node* node); //done
+
+    std::vector<int> toArray() const; //done C
+
+    bool remNodeByVal(const int &val);   //done C
+
+    bool remNodeByPos(const int &pos);   //done
+
+    bool remNode(Node* node);   //done C
+
+    bool clearList();   //done C
+
+    bool isEmpty() const;   //done
+
+    bool swapNodesByVal(const int &val1, const int &val2); //done
+
+    bool swapNodesByPos(const int &pos1, const int &pos2); //done
+
+    bool swapNodes(Node* node1, Node* node2);   //done C
+
+    int getSize() const;  //done
+
+    void printList() const;   //done
+    
+    void reverseList(); //done C
+
+    ~DualyLinkedList();    //done C
 
 };
 
